fc9d6ac70ce4f7195ccf602406dec456f052905b
diegoalvarezg <554716@unizar.es>
2018-05-13 15:37:08 +0200

    Subject: Local Front-end working

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -4,6 +4,8 @@ var newQuery = 0;
 function registerTemplate() {
 	template = $("#template").html();
 	Mustache.parse(template);
+	tt_template = $("#trends_template").html();
+	Mustache.parse(tt_template);
 }
 
 function setConnected(connected) {
@@ -17,6 +19,23 @@ function registerSendQueryAndConnect() {
     stompClient.connect({}, function(frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
+
+        subscriptionTrends = stompClient.subscribe("/queue/trends", function(data) {
+
+            console.log("Subscribed to trendings");
+
+            tt=JSON.parse(data.body);
+            		var array = [];
+            		tt.forEach(function(element) {
+            		    for(var trend in element){
+            		    	array.push({'key':trend,'val':element[trend]});
+
+            		    }
+            		});
+            		console.log({"trends": array})
+            		$("#trendingsBlock").html(Mustache.render(tt_template, {"trends": array}));
+
+        })
     });
 	$("#search").submit(
 			function(event) {
